c67e0346387bee9360f7a77c5c316a74090c8df2
rlaehgus97 <starkim625@gmail.com>
2024-10-07 15:43:46 +0900

    model.py added

---
+++ dags/model/donut.py
@@ -0,0 +1,22 @@
+from donut import DonutModel
+from PIL import Image
+import torch
+
+model = DonutModel.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+
+if torch.cuda.is_available():
+    model.half()
+    device = torch.device("cuda")
+    model.to(device)
+else:
+    device = torch.device("cpu")
+    model.encoder.to(torch.float)
+    model.to(device)
+
+model.eval() 
+
+def process(path):
+    image = Image.open(path).convert("RGB")
+    output = model.inference(image=image, prompt="<s_cord-v2>")
+
+    return output
